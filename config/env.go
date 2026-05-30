16a85512fb7a6af231825fa807b1ca634f8efd3b
verse91 <versedev.store@proton.me>
2026-05-30 21:19:47 +0700

    feat(config): environment overrides

---
+++ config/env.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"os"
+	"strconv"
+	"time"
+)
+
+func applyEnv(cfg *Config) {
+	if val := os.Getenv("IRIS_CORE_DEBUG"); val != "" {
+		if b, err := strconv.ParseBool(val); err == nil {
+			cfg.Core.Debug = b
+		}
+	}
+	if val := os.Getenv("IRIS_CORE_SHELL"); val != "" {
+		cfg.Core.Shell = val
+	}
+	if val := os.Getenv("IRIS_CORE_MODE"); val != "" {
+		cfg.Core.Mode = val
+	}
+	if val := os.Getenv("IRIS_UI_GHOST_TEXT"); val != "" {
+		if b, err := strconv.ParseBool(val); err == nil {
+			cfg.UI.GhostText = b
+		}
+	}
+	if val := os.Getenv("IRIS_UI_MAX_SUGGESTIONS"); val != "" {
+		if i, err := strconv.Atoi(val); err == nil {
+			cfg.UI.MaxSuggestions = i
+		}
+	}
+	if val := os.Getenv("IRIS_UI_MAX_HEIGHT"); val != "" {
+		if i, err := strconv.Atoi(val); err == nil {
+			cfg.UI.MaxHeight = i
+		}
+	}
+	if val := os.Getenv("IRIS_UPDATER_CHANNEL"); val != "" {
+		cfg.Updater.Channel = val
+	}
+	if val := os.Getenv("IRIS_UPDATER_INTERVAL"); val != "" {
+		if dur, err := time.ParseDuration(val); err == nil {
+			cfg.Updater.CheckInterval = Duration(dur)
+		}
+	}
+	if val := os.Getenv("IRIS_UPDATER_CHECK_ON_STARTUP"); val != "" {
+		if b, err := strconv.ParseBool(val); err == nil {
+			cfg.Updater.CheckOnStartup = b
+		}
+	}
+}
