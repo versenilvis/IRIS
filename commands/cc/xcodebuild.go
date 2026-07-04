4dfc31d194d6614a3229d514255ba018d8b9985d
verse91 <versedev.store@proton.me>
2026-07-04 21:35:41 +0700

    feat(commands/cc): add C/C++ compilers and build systems specs

---
+++ commands/cc/xcodebuild.go
@@ -0,0 +1,54 @@
+package cc
+
+import (
+	"github.com/versenilvis/iris/commands/core"
+)
+
+func init() {
+	core.Register(&core.Spec{
+		Name:        "xcodebuild",
+		Description: "Build Xcode projects",
+		Options: []core.Option{
+			{Name: "-usage", Description: "Print brief usage"},
+			{Name: "-help", Description: "Print complete usage"},
+			{Name: "-verbose", Description: "Provide additional status output"},
+			{Name: "-license", Description: "Show the Xcode and SDK license agreements"},
+			{Name: "-checkFirstLaunchStatus", Description: "Check if any First Launch tasks need to be performed"},
+			{Name: "-runFirstLaunch", Description: "Install packages and agree to the license"},
+			{Name: "-project", Description: "Build the project NAME"},
+			{Name: "-target", Description: "Build the target NAME"},
+			{Name: "-alltargets", Description: "Build all targets"},
+			{Name: "-workspace", Description: "Build the workspace NAME"},
+			{Name: "-scheme", Description: "Build the scheme NAME"},
+			{Name: "-configuration", Description: "Use the build configuration NAME for building each target"},
+			{Name: "-xcconfig", Description: "Apply the build settings defined in the file at PATH as overrides"},
+			{Name: "-arch", Description: "Use SDK as the name or path of the base SDK when building the project"},
+			{Name: "-toolchain", Description: "Use the toolchain with identifier or name NAME"},
+			{Name: "-destination", Description: "Wait for TIMEOUT seconds while searching for the destination device"},
+			{Name: "-parallelizeTargets", Description: "Build independent targets in parallel"},
+			{Name: "-jobs", Description: "Specify the maximum number of concurrent build operations"},
+			{Name: "-parallel-testing-enabled", Description: "Overrides the per-target setting in the scheme"},
+			{Name: "-dry-run", Description: "Do everything except actually running the commands"},
+			{Name: "-quiet", Description: "Do not print any output except for warnings and errors"},
+			{Name: "-hideShellScriptEnvironment", Description: "Don't show shell script environment variables in build log"},
+			{Name: "-showsdks", Description: "Display a compact list of the installed SDKs"},
+			{Name: "-showdestinations", Description: "Display a list of destinations"},
+			{Name: "-showTestPlans", Description: "Display a list of test plans"},
+			{Name: "-showBuildSettings", Description: "Display a list of build settings and values"},
+			{Name: "-showBuildSettingsForIndex", Description: "Display build settings for the index service"},
+			{Name: "-list", Description: "Lists the targets and configurations in a project, or the schemes in a workspace"},
+			{Name: "-find-executable", Description: "Display the full path to executable NAME in the provided SDK and toolchain"},
+			{Name: "-find-library", Description: "Display the full path to library NAME in the provided SDK and toolchain"},
+			{Name: "-version", Description: "Turn the address sanitizer on or off"},
+			{Name: "-enableThreadSanitizer", Description: "Turn the thread sanitizer on or off"},
+			{Name: "-resultBundlePath", Description: "Specifies which result bundle version should be used"},
+			{Name: "-clonedSourcePackagesDirPath", Description: "Specifies the directory where build products and other derived data will go"},
+			{Name: "-archivePath", Description: "Specifies that an archive should be exported"},
+			{Name: "-exportNotarizedApp", Description: "Export an archive that has been notarized by Apple"},
+			{Name: "-exportOptionsPlist", Description: "Specifies a path to a plist file that configures archive exporting"},
+			{Name: "-enableCodeCoverage", Description: "Turn code coverage on or off when testing"},
+			{Name: "-exportPath", Description: "Specifies the destination for the product exported from an archive"},
+			{Name: "-skipUnavailableActions", Description: "Exports completed and outstanding project localizations"},
+		},
+	})
+}
---
+++ commands/cc/xcrun.go
@@ -0,0 +1,38 @@
+package cc
+
+import (
+	"github.com/versenilvis/iris/commands/core"
+)
+
+func init() {
+	core.Register(&core.Spec{
+		Name:        "xcrun",
+		Description: "SceneKit CLI utilities",
+		Subcommands: []core.Subcommand{
+			{Name: "scntool", Description: "SceneKit CLI utilities"},
+		},
+		Options: []core.Option{
+			{Name: "--convert", Description: "File to convert"},
+			{Name: "--format", Description: "Format to convert to"},
+			{Name: "--output", Description: "Path to the output file"},
+			{Name: "--force-y-up", Description: "Convert objects to use y axis up"},
+			{Name: "--force-interleaved", Description: "Use .ktx, .astc and .pvrtc files for textures if available in the asset catalog"},
+			{Name: "--verbose", Description: "Get pretty error message"},
+			{Name: "-h", Description: "Help message"},
+			{Name: "--version", Description: "Show the xcrun version"},
+			{Name: "-v", Description: "Show verbose logging output"},
+			{Name: "--sdk", Description: "Find the tool for the given SDK name"},
+			{Name: "--toolchain", Description: "Find the tool for the given toolchain"},
+			{Name: "-l", Description: "Show command path to be executed (and --run)"},
+			{Name: "-f", Description: "Find and print the tool path"},
+			{Name: "--run", Description: "(Default) find and execute the tool"},
+			{Name: "-n", Description: "Do not use the lookup cache"},
+			{Name: "-k", Description: "Invalidate all existing cache entries"},
+			{Name: "--show-sdk-path", Description: "Show selected SDK install path"},
+			{Name: "--show-sdk-version", Description: "Show selected SDK version"},
+			{Name: "--show-sdk-build-version", Description: "Show selected SDK build version"},
+			{Name: "--show-sdk-platform-path", Description: "Show selected SDK platform path"},
+			{Name: "--show-sdk-platform-version", Description: "Show selected SDK platform version"},
+		},
+	})
+}
