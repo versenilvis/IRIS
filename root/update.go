9af2df51b6738d3107870d416ab42cab34d8c6c3
verse91 <versedev.store@proton.me>
2026-07-28 10:59:31 +0700

    chore(update): remove unused pendingUpdate global variable

--- root/update.go
+++ root/update.go
@@ -22,9 +22,6 @@ type updateResult struct {
 	hasUpdate     bool
 }
 
-// pendingUpdate is set by the background goroutine and consumed once after the first IRIS_CMD_STOP
-var pendingUpdate chan updateResult
-
 // FetchLatestVersion hits the GitHub Releases API and returns the latest tag name
 func FetchLatestVersion() (string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
